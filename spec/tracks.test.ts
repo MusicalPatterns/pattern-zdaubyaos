972c3f0ae7fbd9cad5fe9264016d34c86e57ce0d
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-08-06 09:04:22 -0700

    oops fix length

--- spec/tracks.test.ts
+++ spec/tracks.test.ts
@@ -3,7 +3,7 @@ import { zdaubyaosTrackA, zdaubyaosTrackB, zdaubyaosTrackC } from '../src/tracks
 
 describe('tracks', () => {
     it('is the case that they all have the same duration', () => {
-        const expectedTotalSongDuration: number = 2496
+        const expectedTotalSongDuration: number = 2712
         expect(calculateDuration(zdaubyaosTrackA)).toEqual(expectedTotalSongDuration)
         expect(calculateDuration(zdaubyaosTrackB)).toEqual(expectedTotalSongDuration)
         expect(calculateDuration(zdaubyaosTrackC)).toEqual(expectedTotalSongDuration)
