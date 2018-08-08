a0e986703dc2d14fff533559513ba25b1630665f
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-08-08 11:37:40 -0700

    voice enums

--- src/entities.ts
+++ src/entities.ts
@@ -1,5 +1,5 @@
 import { harmonicSeriesPitches, subharmonicSeriesPitches } from '../../../src/pitches'
-import { EntityConfig } from '../../../src/types'
+import { EntityConfig, OscillatorName, VoiceType } from '../../../src/types'
 import {
     zdaubyaosTrackA,
     zdaubyaosTrackB,
@@ -20,42 +20,42 @@ const TO_AVOID_BLOW_OUT: number = 0.2
 const zdaubyaosSubparticularSquare: EntityConfig = {
     notes: zdaubyaosTrackA,
     pitches: subparticularSeriesPitches,
-    voiceConfig: {timbre: 'square', voiceType: 'oscillator'},
+    voiceConfig: {timbre: OscillatorName.SQUARE, voiceType: VoiceType.OSCILLATOR},
     voiceGain: TO_AVOID_BLOW_OUT,
 }
 
 const zdaubyaosDubparticularSaw: EntityConfig = {
     notes: zdaubyaosTrackB,
     pitches: dubparticularSeriesPitches,
-    voiceConfig: {timbre: 'sawtooth', voiceType: 'oscillator'},
+    voiceConfig: {timbre: OscillatorName.SAWTOOTH, voiceType: VoiceType.OSCILLATOR},
     voiceGain: TO_AVOID_BLOW_OUT,
 }
 
 const zdaubyaosHarmonicSine: EntityConfig = {
     notes: zdaubyaosTrackC,
     pitches: harmonicSeriesPitches,
-    voiceConfig: {timbre: 'sine', voiceType: 'oscillator'},
+    voiceConfig: {timbre: OscillatorName.SINE, voiceType: VoiceType.OSCILLATOR},
     voiceGain: TO_AVOID_BLOW_OUT,
 }
 
 const zdaubyaosSuperparticularSquare: EntityConfig = {
     notes: zdaubyaosTrackD,
     pitches: superparticularSeriesPitches,
-    voiceConfig: {timbre: 'square', voiceType: 'oscillator'},
+    voiceConfig: {timbre: OscillatorName.SQUARE, voiceType: VoiceType.OSCILLATOR},
     voiceGain: TO_AVOID_BLOW_OUT,
 }
 
 const zdaubyaosDuperparticularSaw: EntityConfig = {
     notes: zdaubyaosTrackE,
     pitches: duperparticularSeriesPitches,
-    voiceConfig: {timbre: 'sawtooth', voiceType: 'oscillator'},
+    voiceConfig: {timbre: OscillatorName.SAWTOOTH, voiceType: VoiceType.OSCILLATOR},
     voiceGain: TO_AVOID_BLOW_OUT,
 }
 
 const zdaubyaosSubharmonicSine: EntityConfig = {
     notes: zdaubyaosTrackF,
     pitches: subharmonicSeriesPitches,
-    voiceConfig: {timbre: 'sine', voiceType: 'oscillator'},
+    voiceConfig: {timbre: OscillatorName.SINE, voiceType: VoiceType.OSCILLATOR},
     voiceGain: TO_AVOID_BLOW_OUT,
 }
 
