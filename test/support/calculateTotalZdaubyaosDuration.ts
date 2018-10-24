ad12e3379863c45791a3b273ab6d2c46bcee4d7c
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-10-24 11:20:42 +0900

    accurate typing of sums of indices and scalars

--- test/support/calculateTotalZdaubyaosDuration.ts
+++ test/support/calculateTotalZdaubyaosDuration.ts
@@ -1,14 +1,17 @@
 import { NoteSpec, NoteSpecs } from '../../../../src/compile/types'
 import * as from from '../../../../src/utilities/from'
-import { Index } from '../../../../src/utilities/nominalTypes'
+import { Index, SumOfIndices } from '../../../../src/utilities/nominalTypes'
 import * as to from '../../../../src/utilities/to'
 
-const calculateTotalZdaubyaosDuration: (noteSpecs: NoteSpecs) => Index =
-    (noteSpecs: NoteSpecs): Index =>
+const calculateTotalZdaubyaosDuration: (noteSpecs: NoteSpecs) => SumOfIndices =
+    (noteSpecs: NoteSpecs): SumOfIndices =>
         noteSpecs.reduce(
-            (m: Index, { durationSpec }: NoteSpec) =>
-                to.Index(from.Index(m) + from.Index(durationSpec && durationSpec.index || to.Index(0)) + 1),
-            to.Index(0),
+            (m: SumOfIndices, { durationSpec }: NoteSpec) => {
+                const durationIndex: Index = durationSpec && durationSpec.index || to.Index(0)
+
+                return to.SumOfIndices(from.SumOfIndices(m) + from.Index(durationIndex) + 1)
+            },
+            to.SumOfIndices(0),
         )
 
 export default calculateTotalZdaubyaosDuration
