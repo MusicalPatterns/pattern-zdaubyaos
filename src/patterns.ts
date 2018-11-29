69886d906008eea172062deaf417e05d76622ada
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-11-28 19:24:01 -0800

    extract cli and registry from shared

--- src/patterns.ts
+++ src/patterns.ts
@@ -1,4 +1,5 @@
-import { Pattern, PatternId, PatternMaterial, PatternMetadata, PatternSpec } from '@musical-patterns/shared'
+import { PatternId } from '@musical-patterns/registry'
+import { Pattern, PatternMaterial, PatternMetadata, PatternSpec } from '@musical-patterns/shared'
 import { ZDAUBYAOS_DURATION_SCALAR, ZDAUBYAOS_PITCH_SCALAR } from './constants'
 import { buildEntities, buildScales } from './materials'
 
