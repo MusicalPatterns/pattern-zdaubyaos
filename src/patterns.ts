1eca88d0cb710f53a15db0f600f20611d97759b0
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-20 11:38:01 -0800

    1.0.52: simplerpatterntypenames

--- src/patterns.ts
+++ src/patterns.ts
@@ -1,16 +1,16 @@
-import { PatternMaterial } from '@musical-patterns/compiler'
-import { PatternMetadata } from '@musical-patterns/pattern'
+import { Material } from '@musical-patterns/compiler'
+import { Metadata } from '@musical-patterns/pattern'
 import { buildPatterns, PatternId, Patterns, StandardPattern } from '@musical-patterns/registry'
 import { buildEntities, buildScales } from './materials'
 import { post } from './metadata'
 import { specData } from './specs'
 
-const material: PatternMaterial = {
+const material: Material = {
     buildEntitiesFunction: buildEntities,
     buildScalesFunction: buildScales,
 }
 
-const metadata: PatternMetadata = {
+const metadata: Metadata = {
     description: post,
     formattedName: 'Zdaubyaos',
     mostRecentPublish: process.env.PUBLISH_DATE || '2018-08-04T07:00:00.000Z',
