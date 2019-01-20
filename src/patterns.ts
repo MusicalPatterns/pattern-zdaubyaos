71ff3220d32dcab609bcf43b18dcefb67139a7f7
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-20 13:41:21 -0800

    1.0.54: patternid

--- src/patterns.ts
+++ src/patterns.ts
@@ -1,6 +1,6 @@
 import { Material } from '@musical-patterns/compiler'
 import { Metadata } from '@musical-patterns/pattern'
-import { buildPatterns, PatternId, Patterns, StandardPattern } from '@musical-patterns/registry'
+import { buildPatterns, Id, Patterns, StandardPattern } from '@musical-patterns/registry'
 import { buildEntities, buildScales } from './materials'
 import { post } from './metadata'
 import { specData } from './specs'
@@ -19,14 +19,14 @@ const metadata: Metadata = {
 }
 
 const pattern: StandardPattern = {
+    id: Id.ZDAUBYAOS,
     material,
     metadata,
-    patternId: PatternId.ZDAUBYAOS,
     specData,
 }
 
 const patterns: Patterns = buildPatterns({
-    [ pattern.patternId ]: pattern,
+    [ pattern.id ]: pattern,
 })
 
 export {
