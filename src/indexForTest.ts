b0d15770c369b395e034901f6a3714c09545332b
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-03-29 21:19:17 -0700

    1.0.125: exclude snapshot from package

--- src/indexForTest.ts
+++ src/indexForTest.ts
@@ -24,8 +24,3 @@ export {
     ThirtyfiveContourPieces,
 } from './material/indexForTest'
 export { pattern } from './patterns'
-
-// tslint:disable-next-line no-default-import
-import * as snapshot from './snapshot.json'
-
-export { snapshot }
